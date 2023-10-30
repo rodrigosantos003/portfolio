ffa3c33de3c41258f4f1c1578f22e31efac0166a
Rodrigo Santos <rodrigo_santos003@outlook.com>
2023-10-30 20:31:33 +0000

    Fix build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,11 @@ import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react';
 import './global.css'
 import NavBar from '@/components/Navbar/Navbar'
-import { initGA } from '@/data/GoogleAnalytics';
 
 export const metadata: Metadata = {
   title: 'Rodrigo Santos',
   description: 'Software Engineer',
 }
-
-initGA();
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useEffect } from 'react';
 import ReactGA from 'react-ga';
 
