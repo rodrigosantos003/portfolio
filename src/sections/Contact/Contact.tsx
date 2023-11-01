83e74c4582f0a93b48a51efadf24c0be78ac4fec
Rodrigo Santos <rodrigo_santos003@outlook.com>
2023-11-01 14:16:13 +0000

    Update LinkedIn link

--- src/sections/Contact/Contact.tsx
+++ src/sections/Contact/Contact.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
 
         <div className="social-links">
             <a href='mailto:rodrigo.s.santos003@gmail.com'>Email</a>
-            <a href='https://linkedin.com/in/rodrigosantos003'>LinkedIn</a>
+            <a href='www.linkedin.com/in/rodrigo-santos-2003'>LinkedIn</a>
             <a href='https://github.com/rodrigosantos003'>GitHub</a>
         </div>
 
