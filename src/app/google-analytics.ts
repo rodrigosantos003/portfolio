5a783721896aa6612b7757fbbefbad1e368ab8bf
Rodrigo Santos <rodrigo_santos003@outlook.com>
2023-11-02 12:40:43 +0000

    Trying react-ga4

--- src/app/google-analytics.ts
+++ src/app/google-analytics.ts
@@ -1,10 +1,10 @@
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 export const initGA = () => {
-    ReactGA.initialize('G-3728W3QDVY'); // Replace with your tracking ID
+    ReactGA.initialize("G-3728W3QDVY"); // Replace with your tracking ID
 };
 
 export const logPageView = () => {
     ReactGA.set({ page: window.location.pathname });
-    ReactGA.pageview(window.location.pathname);
+    ReactGA.send({ hitType: "pageview", page: "/", title: "Home" });
 };
