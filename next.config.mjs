16c1b821fb8b92c1a50bc9cefc82183976bcd28f
Rodrigo Santos <rodrigo_santos003@outlook.com>
2025-03-28 12:16:28 +0000

    Update project cards

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    devIndicators: {
-        buildActivity: false,
-        appIsrStatus: false
-    }
 };
 
 export default nextConfig;
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({ data }) => {
         let result = ''
 
         words.forEach((word) => {
-            if (word.length === 2 || word.length === 3)
+            if (word.length <= 3)
                 result += word.toUpperCase() + ' '
             else result += word.charAt(0).toUpperCase() + word.slice(1) + ' '
         })
