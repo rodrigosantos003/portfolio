e1d490f8ec0451e47ed16515cbd887a752c11016
Rodrigo Santos <rodrigo_santos003@outlook.com>
2025-07-21 13:01:50 +0100

    refactor: Moved projects into a scrollable div

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,58 +1,61 @@
 "use client";
 
-import { useEffect, useState } from 'react';
-import './Projects.css';
-import Card from '../../components/Card/Card';
-import data from '../../data.json'
+import { useEffect, useState } from "react";
+import "./Projects.css";
+import Card from "../../components/Card/Card";
+import data from "../../data.json";
 
 const Projects = () => {
-    const [repos, setRepos] = useState([]);
-    const [limit, setLimit] = useState(7);
+  const [repos, setRepos] = useState([]);
+  const [limit, setLimit] = useState(6);
 
-    useEffect(() => {
-        // Load repos from GitHub
-        fetch('https://api.github.com/users/rodrigosantos003/repos')
-            .then(res=>res.json())
-            .then(repoData => {
+  useEffect(() => {
+    // Load repos from GitHub
+    fetch("https://api.github.com/users/rodrigosantos003/repos")
+      .then((res) => res.json())
+      .then((repoData) => {
+        const sortedData = repoData.sort(
+          (a, b) => new Date(b.created_at) - new Date(a.created_at)
+        );
+        setRepos(sortedData);
+      })
+      .catch((error) => {
+        console.error("Error fetching repos: ", error);
+      });
+  }, []);
 
-                // Sort the data by created_at date
-                const sortedData = repoData.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
-                setRepos(sortedData);
-            })
-            .catch((error) => {
-                console.error("Error fetching repos: ", error);
-        })
-    }, [])
+  const currentData = repos.slice(0, limit);
 
-    const currentData = repos.slice(0, limit);
+  const handleLoadMore = () => {
+    setLimit((prevLimit) => prevLimit * 2);
+  };
 
-    const handleLoadMore = () => {
-        setLimit(prevLimit => prevLimit * 2);
-    };
-
-    return (
-        <section id='Projects'>
-            <h1>Projects</h1>
-            {currentData.length > 0 ? <>
-            <div className='card-grid'>
-                {currentData.map((repo) => (
-                    !data.ignoredRepos.includes(repo.name) && (
-                        <Card key={repo.id} data={repo} />
-                    )
-                ))}
+  return (
+    <section id="Projects">
+      <h1>Projects</h1>
+      {currentData.length > 0 ? (
+        <>
+          <div className="card-grid scrollable">
+            {currentData.map(
+              (repo) =>
+                !data.ignoredRepos.includes(repo.name) && (
+                  <Card key={repo.id} data={repo} />
+                )
+            )}
+          </div>
+          {currentData.length < repos.length && (
+            <div className="view-more-container">
+              <button onClick={handleLoadMore} className="view-more">
+                View More
+              </button>
             </div>
-            {currentData.length < repos.length && (
-                <div className='view-more-container'>
-                    <button onClick={handleLoadMore} className='view-more'>
-                        View More
-                    </button>
-                </div>
-                )}
-                </>
-
-        : <p>No data available</p>}
-        </section>
-    );
-}
+          )}
+        </>
+      ) : (
+        <p>No data available</p>
+      )}
+    </section>
+  );
+};
 
 export default Projects;
